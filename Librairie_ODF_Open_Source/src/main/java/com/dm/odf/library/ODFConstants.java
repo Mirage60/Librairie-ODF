9442d0a446092ce09bf9746a4c69298ebed37293
Didier <sepecat@wanadoo.fr>
2017-10-11 23:00:02 +0200

    Chargement initial.

---
+++ Librairie_ODF_Open_Source/src/main/java/com/dm/odf/library/ODFConstants.java
@@ -0,0 +1,17 @@
+package com.dm.odf.library;
+
+public final class ODFConstants
+{
+
+	//==========================================================================
+	// CONSTRUCTEURS
+	//==========================================================================
+
+	private ODFConstants()
+	{
+
+		super();
+
+	}
+
+}
